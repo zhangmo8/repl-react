6b59bdc0e97d3b926701f7deec3a359d6c3c75e7
zhangmo8 <wegi866@gmail.com>
2024-11-25 18:03:30 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
+        dir: "build",
         chunkFileNames: "chunks/[name]-[hash].js",
         globals: {
           react: "React",
